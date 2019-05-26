4665f3114e4b0fb3fc6ed215fe330eeeb369b82a
ridhodarman <ridhodarman@gmail.com>
2019-05-26 14:02:56 +0700

    id kes

--- pages/kesehatan/act/tambah-fasilitas.php
+++ pages/kesehatan/act/tambah-fasilitas.php
@@ -1,20 +1,23 @@
 <?php
-	session_start();
-    if(isset($_SESSION['username']) && $_POST['fasilitas'] != null ) {
-
-		include ('../../../inc/koneksi.php');
-
-		$result = pg_query("SELECT MAX(facility_id) AS id FROM health_building_facilities");
-	    $row = pg_fetch_array($result);
-	    $id = $row["id"];
-	    $id2 = $id+1;
-
-		$fas = $_POST['fasilitas'];
-
-		$sql = pg_query("INSERT INTO health_building_facilities (facility_id, name_of_facility) VALUES ('$id2', '$fas')");
+session_start();
+if(isset($_SESSION['username']) && $_POST['fasilitas'] != null ) {
+	include ('../../../inc/koneksi.php');
+	$result = pg_query("SELECT facility_id AS id FROM health_building_facilities");
+    $id=0;
+	while ($data=pg_fetch_assoc($result)) {
+		echo $temp=str_replace("F", "", $data["id"]);
+		if ($id<$temp) {
+			$id=$temp;
+		}
 	}
-
-	else {
-		echo '<script>window.location="../../../assets/403"</script>';
-	}
+    $id = $id+1;
+    if ($id<10) {
+    	$id2="F0".$id;
+    }
+    else {
+    	$id2="F".$id;
+    }
+	$fas = $_POST['fasilitas'];
+	$sql = pg_query("INSERT INTO health_building_facilities (facility_id, name_of_facility) VALUES ('$id2', '$fas')");
+}
 ?>
--- pages/kesehatan/act/tambah-jenis.php
+++ pages/kesehatan/act/tambah-jenis.php
@@ -1,16 +1,24 @@
 <?php
-	session_start();
-    if(isset($_SESSION['username']) && $_POST['jenis'] != null ) {
-		include ('../../../inc/koneksi.php');
-		$result = pg_query("SELECT MAX(type_id) AS id FROM type_of_health_building");
-	    $row = pg_fetch_array($result);
-	    $id = $row["id"];
-	    $id2 = $id+1;
-		$jenis = $_POST['jenis'];
+session_start();
+if(isset($_SESSION['username']) && $_POST['jenis'] != null ) {
+	include ('../../../inc/koneksi.php');
+	$result = pg_query("SELECT type_id AS id FROM type_of_health_building");
+    $id=0;
+	while ($data=pg_fetch_assoc($result)) {
+		echo $temp=str_replace("H", "", $data["id"]);
+		if ($id<$temp) {
+			$id=$temp;
+		}
+	}
+    $id = $id+1;
+    if ($id<10) {
+    	$id2="H0".$id;
+    }
+    else {
+    	$id2="H".$id;
+    }
+	$jenis = $_POST['jenis'];
 
-		$sql = pg_query("INSERT INTO type_of_health_building (type_id, name_of_type) VALUES ('$id2', '$jenis')");
-	}
-	else {
-		echo '<script>window.location="../../../assets/403"</script>';
-	}
+	$sql = pg_query("INSERT INTO type_of_health_building (type_id, name_of_type) VALUES ('$id2', '$jenis')");
+}
 ?>
